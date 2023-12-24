1a8d7ce661a4649f38960e4952a3d35d66f17689
Xauan7x <154389960+Xauan7x@users.noreply.github.com>
2023-12-24 19:38:52 +0000

    Pending changes exported from your codespace

---
+++ import time.py
@@ -0,0 +1,101 @@
+import time
+import loading
+import random
+import json,requests
+import telebot
+from  datetime import deletime
+from  telebot.types import InlineKeyboardMar
+import bd
+from pytz import timezone
+
+aapikey = '6665816565:AAEHxXS0eNG7xaxU7ozrltpt4-wHilZ5H9s
+chat_id = '-1002022301496'  # SEU ID #####
+bot = telebot.TeleBot(token=api_key)
+
+message_ids1 = Tru
+message_delet1 = False
+dados = True
+
+def ALERT_GALE1():
+        tz = timezone('America/São_Paulo')
+         now = datetime.now(tz))
+         h = datetime.now().bour
+         m = datetime.now().minute+1
+         s = datetime.now().second
+
+         if m > 59:
+             h +=  1
+             m = 0
+
+             if h <= 9:
+                 h = f'0{h}'
+             if m <= 9:
+                 m = f'0(m)'
+             if s <= 9:
+                 s = f'0{s}'
+             message_id = bot.send_message(chat_id
+             ✅️)Gerando Oportunidades ''').message
+             message_ids1 = memessagid
+             mensage_delet = False
+             retun
+
+Def DELET_GALE1():
+             if  bd.mensage_delet1 == True
+                  bot.delet_message(chat_id=chat_id)
+                  bd.mensage_delet1 = False
+
+while True:
+   tz = timezone('America/São_Paulo')
+   noow = datetime.now(tz)
+   h = datetime.now().hour
+   m = datetime.now().minute+4
+   s = datetime.now().second
+
+   If m > 59:
+       h += 1
+       m = 0
+
+   If h <= 9:
+       h = f '0{h}'
+   If s <= 9:
+       s = f'0{s}')
+   print(f'{h} : {m} : {s}')
+   #honra = datetime.datetime.now().strtime
+   cores = ['🟦','🟦','🟦','⭐️','🟦','🟦','🟦','⭐️','🟦','🟦','🟦','⭐️','🟦','🟦','🟦','⭐️','🟦','🟦','🟦','⭐️','🟦','🟦','🟦','⭐️','🟦
+
+   for i in range(25)
+       sample = ramdow.sample(cores, k=25)
+       print(sample[0],  sample[1], sample[2], sample[3], sample[4], sample[5], sample[6], sample[7], sample[8], sample[9], sample[10], sample[11], sample[12], sample[13], sample[14], sample[15], sample[16], sample[17], sample[18], sample[19], sample[20], sample[21], sample[22], sample[23], sample[24], sample[25]
+       
+   def button_link():
+       
+       markup = IlineKeyboardButton(text
+       return markup
+     dados =bot.send_message(chat_id=chat_id
+  .)💎 Entrada Confirmada 💎
+  🎯 Entrada:
+
+  {ramdom.choice(sample[0])}{ramdom.choice(sample[1])}{ramdom.choice(sample[8])}{ramdom.choice(sample[16])}{ramdom.choice(sample[23])}
+  {ramdom.choice(sample[5])}{ramdom.choice(sample[2])}{ramdom.choice(sample[9]])}{ramdom.choice(sample[17])}{ramdom.choice(sample[24])}
+  {ramdom.choice(sample[10])}{ramdom.choice(sample[3])}{ramdom.choice(sample[11])}{ramdom.choice(sample[18])}
+  {ramdom.coice(sample[15])}{ramdom.choice(sample[4])}{ramdom.choice(sample[12])}{ramdom.choice(sample[19])}
+  {ramdom.choice(sample[20])}{ramdom.choice(sample[6])}{ramdom.choice(sample[13])}{ramdom.choice(sample[21])}
+  {ramdom.choice(sample[25])}{ramdom.choice(sample[7])}{ramdom.choice(sample[14])}{ramdom.choice(sample[22])}
+
+
+  🕹 Tentativas 3
+  🎰  Plataforma : NN5
+  ⏰️ Valido por 2 minutos''')
+      reply_markup=button_link())
+      time.sleep(240)#240
+
+      bot .sendmesage(chat_id, texte=
+   💎Sinal Confirmado💎
+   ✅️ GREEN ✅️
+   Finalizado
+   ''', dados . chat . id , dados . message_id
+       time.sleep(60)#60
+       ##ALERT_GALE1()
+       time.sleep(10)#10
+       #DELET_GALE1()
+       time.sleep(50)#50
---
+++ tutorial.py
@@ -0,0 +1,19 @@
+import time
+import random
+import json
+import telebot
+from datetime import datetime
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+import bd
+
+api_key = ``
+chat_id = 
+
+bot = telebot.Telebot(token=api_key)
+def ALERT_GALE1()
+    h = datetime.now().hour
+    M = datetime.now().minute + 1
+    s = datetime.now().second
+    if h <= 9
+        h = f``
+                      
